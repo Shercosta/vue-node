8fc271f2dbe06b03016ae48242f224fb27b65ade
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2024-01-02 17:30:53 +0700

    initial

---
+++ vue-project/src/App.vue
@@ -0,0 +1,52 @@
+<!-- <script setup> -->
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My first Vue from Export'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
